2f1921bd6796582c4280591a834e9a8d73faf2db
Fredrik Boegard <fredrik@boegard.com>
2023-01-07 17:34:58 +0100

    Update start.js

--- start.js
+++ start.js
@@ -1,2 +1,7 @@
 let d = new Date();
-document.body.innerHTML += "<h2>Today's date is " + d + "</h2>"
+let datetext = "<h2>Today's date is " + d + "</h2>"
+
+window.onload = function() {
+        document.getElementById("h1_Text").innerHTML = datetext;
+    }
+
