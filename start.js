fcb4f1feb8702ee11ce8d0bee994ff245b2bed09
Fredrik Boegard <fredrik@boegard.com>
2023-01-07 17:43:02 +0100

    Update start.js

--- start.js
+++ start.js
@@ -2,6 +2,7 @@ let d = new Date();
 let datetext = "<h2>Today's date is " + d + "</h2>";
 
 window.onload = function() {
-        document.getElementById("h1_Text").innerHTML = datetext;
+        document.querySelector("#h1_Text").innerHTML = "HELLO";
+        //document.getElementById("h1_Text").innerHTML = datetext;
     }
 
